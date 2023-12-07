d5b58fe7a249015227056cbce21cf12caabb4486
nkartikaxioned <kartikn@axioned.com>
2023-12-07 11:01:47 +0530

    basic task code add

--- phpBasicTask.php
+++ phpBasicTask.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+
+    //1) Write a PHP script to get the PHP version and configuration information.
+    echo "<h2>PHP script to get the PHP version and configuration information.</h2>";
+    echo phpinfo()."</br>";
+    echo "<h2>PHP script to get the PHP version and configuration information.</h2>";
+    echo phpversion()."</br>";
+
+    /*2)Write a PHP script to display the following strings.
+Sample String : 
+'Tomorrow I \'ll learn PHP global variables.'
+'This is a bad command : del c:\\*.*' */
+     echo 'Tomorrow I \'ll learn PHP global variables.'."</br>";
+     print 'This is a bad command : del c:\\*.*'."</br>";
+     
+    ?>
 </body>
 </html>
